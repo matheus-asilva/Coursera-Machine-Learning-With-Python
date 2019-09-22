ec91849de22a8afe3b3427e7a4edee6d1d01a7ae
Matheus de Almeida Silva <ms.asilvas1@gmail.com>
2019-09-21 21:53:05 -0300

    Finished Week 1

--- Week 1/ex1.py
+++ Week 1/ex1.py
@@ -2,12 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-path = 'Week 1/Data/'
+path = 'Data/'
 
 data = pd.read_csv(path + 'ex1data1.txt', sep=',', names=['X1', 'X2'])
 
-plt.scatter(x = data['X1'], y=data['X2'])
-plt.show()
+m = len(data)
 
 X = np.stack([np.ones(m), data['X1']], axis = 1)
 y = data['X2'].copy()
@@ -18,7 +17,7 @@ def cost(X, y, theta):
         theta = np.array(theta)
     return np.sum((X @ theta.T - y) ** 2)/(2 * m)
 
-def gradient_descent(X, y, theta, alpha = 0.01, iter = 1500):
+def gradient_descent(X, y, theta, alpha = 0.01, iter = 3000):
     m = len(X)
     theta = theta.copy()
     J_hist = []
@@ -37,6 +36,11 @@ def gradient_descent(X, y, theta, alpha = 0.01, iter = 1500):
 theta = np.zeros(2)
 
 theta, J_hist = gradient_descent(X, y, theta)
+
+plt.plot(np.linspace(1, len(J_hist), num=3000), J_hist)
+plt.xlabel('Number of Iterations')
+plt.ylabel('Cost $J(\\theta_0, \\theta_1)$')
+plt.show()
 
 plt.scatter(X[:,1], y)
 plt.plot(X[:,1], np.dot(X, theta), '-', color='r')
